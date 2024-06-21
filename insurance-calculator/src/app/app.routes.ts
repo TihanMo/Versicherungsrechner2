27ede1b8d7a84426983476846886f6adad231a3a
TihanMo <tihan.morrol@lernende.bbw.ch>
2024-06-21 10:21:58 +0200

    login and sign in html css added

--- insurance-calculator/src/app/app.routes.ts
+++ insurance-calculator/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { InsuranceFormComponent } from './insurance-form/insurance-form.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { DatenschutzComponent } from './datenschutz/datenschutz.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'calculate', component: InsuranceFormComponent },
   { path: 'impressum', component: ImpressumComponent },
   { path: 'datenschutz', component: DatenschutzComponent },
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'login', component: LoginComponent },
 ];
