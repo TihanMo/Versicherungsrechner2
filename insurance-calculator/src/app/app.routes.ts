84c4a718262de8efae37b76cf7414747ddefb03f
TihanMo <tihan.morrol@lernende.bbw.ch>
2024-06-21 09:29:16 +0200

    datenschutz and impressum added

--- insurance-calculator/src/app/app.routes.ts
+++ insurance-calculator/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { InsuranceFormComponent } from './insurance-form/insurance-form.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { DatenschutzComponent } from './datenschutz/datenschutz.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'calculate', component: InsuranceFormComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'datenschutz', component: DatenschutzComponent },
 ];
