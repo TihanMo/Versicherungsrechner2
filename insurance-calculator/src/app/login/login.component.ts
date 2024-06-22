59cf9605042e8974998f6579013699ad2c003f50
TihanMo <tihan.morrol@lernende.bbw.ch>
2024-06-22 09:05:50 +0200

    added loading to login, sign-in

--- insurance-calculator/src/app/login/login.component.ts
+++ insurance-calculator/src/app/login/login.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  showLoading(event: Event) {
+    event.preventDefault();
+    const button = (event.target as HTMLFormElement).querySelector('button');
+    if (button) {
+      button.textContent = 'Lädt...';
+      button.disabled = true;
+      setTimeout(() => {
+        button.textContent = 'Anmelden';
+        button.disabled = false;
+      }, 2000);
+    }
+  }
 }
--- insurance-calculator/src/app/sign-in/sign-in.component.ts
+++ insurance-calculator/src/app/sign-in/sign-in.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
-
+  showLoading(event: Event) {
+    event.preventDefault();
+    const button = (event.target as HTMLFormElement).querySelector('button');
+    if (button) {
+      button.textContent = 'Lädt...';
+      button.disabled = true;
+      setTimeout(() => {
+        button.textContent = 'Anmelden';
+        button.disabled = false;
+      }, 2000);
+    }
+  }
 }
